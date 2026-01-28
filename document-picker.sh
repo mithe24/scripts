83107c60c0a1135e4548d588eef7d94b2adaa1af
Mikkel Thestrup <mikkel@mithe.dk>
2026-01-28 22:38:27 +0100

    Script for opening documents using dmenu

---
+++ document-picker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Utility for opening documents using dmenu.
+
+DOCS="$HOME/documents"
+
+choice=$(find "$DOCS" -type f 2>/dev/null \
+    | sed "s|^$DOCS/||" \
+    | dmenu -i -l 15 -p "Documents:")
+
+[ -z "$choice" ] && exit 0
+
+xdg-open "$DOCS/$choice" >/dev/null 2>&1 &
