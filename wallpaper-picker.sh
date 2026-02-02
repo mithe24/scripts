9a0e1af9a726b0f3f8c8a4b4d19034180c0d37c7
Mikkel Thestrup <mikkel@mithe.dk>
2026-02-02 11:17:55 +0100

    Remove unnecessary case statement

--- wallpaper-picker.sh
+++ wallpaper-picker.sh
@@ -3,27 +3,13 @@
 
 WALLPAPER_DIR="$HOME/pictures/wallpaper"
 
-pick_wallpaper() {
-    choice=$(find "$WALLPAPER_DIR" \
-        -maxdepth 1 \
-        -type f \
-        -printf '%f\n' \
-        2>/dev/null \
-        | dmenu -i -l 5 -p "Wallpaper:")
+choice=$(find "$WALLPAPER_DIR" \
+    -maxdepth 1 \
+    -type f \
+    -printf '%f\n' \
+    2>/dev/null \
+    | dmenu -i -l 5 -p "Wallpaper:")
 
-    [ -n "$choice" ] || exit 0
+[ -n "$choice" ] || exit 0
 
-    wallpaper "$WALLPAPER_DIR/$choice"
-}
-
-case $# in
-    0)
-        pick_wallpaper
-        ;;
-    1)
-        wallpaper "$1"
-        ;;
-    *)
-        exit 1
-        ;;
-esac
+wallpaper "$WALLPAPER_DIR/$choice"
